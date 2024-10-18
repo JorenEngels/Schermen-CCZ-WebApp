e71f59994cfd518e5c0909bdcc8a7334e6811dd2
JorenEngels <dj.joren.engels@gmail.com>
2024-10-18 16:00:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
-// Tekstbestanden correct koppelen
-async function loadText(filename, elementId) {
-    try {
-        let response = await fetch(filename);
-        let text = await response.text();
-        document.getElementById(elementId).innerText = text;
-    } catch (error) {
-        document.getElementById(elementId).innerText = 'Geen informatie';
-    }
-}
+window.onload = function() {
+    // Ophalen van de tekstbestanden en invullen in de HTML elementen
+    fetch('./Date1.txt')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('datum').innerText = data;
+        });
 
-// Correcte tekstbestanden koppelen
-loadText('Date1.txt', 'datum');
-loadText('Meal1.txt', 'maaltijd');
-loadText('Naam1.txt', 'naam');
-loadText('Starttime1.txt', 'starttijd');
-loadText('Time1.txt', 'tijd');
-loadText('TimeSchedule1.txt', 'tijdschaal');
-loadText('Publieksaantal1.txt', 'publieksaantal');
+    fetch('./Meal1.txt')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('maaltijd').innerText = data;
+        });
 
-// Afbeelding correct plaatsen
-document.getElementById('image').src = 'Image1.jpg';
+    fetch('./Naam1.txt')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('naam').innerText = data;
+        });
+
+    fetch('./Starttime1.txt')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('starttijd').innerText = data;
+        });
+
+    fetch('./Time1.txt')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('tijd').innerText = data;
+        });
+
+    fetch('./TimeSchedule1.txt')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('tijdschaal').innerText = data;
+        });
+
+    fetch('./Publieksaantal1.txt')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('publieksaantal').innerText = data;
+        });
+};
